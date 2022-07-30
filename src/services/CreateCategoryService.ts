8d46923b49681a6f98a690a005c6aaf047eff3f9
Matheus Marques Cunha <matheus.overseas@gmail.com>
2022-07-29 22:06:27 -0300

    RentalX

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,31 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [X] Definir o tipo de retorno
+ * [X] Alterar o retorno de error
+ * [X] acessar o repositorio
+ * [X] Retornar algo
+ */
+class CreateCategoryService {
+
+  constructor(private categoriesRepository: CategoriesRepository) { }
+
+  execute({ name, description }: IRequest): void {
+
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error("Category already Exists")
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService }
