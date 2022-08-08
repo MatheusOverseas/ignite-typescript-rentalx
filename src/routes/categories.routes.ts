493d7f09225391900092c92824e1d6ae79102414
MatheusOver <matheus.overseas@gmail.com>
2022-08-08 12:07:04 -0300

    RentalX

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
-
+import { listCategoriesController } from "../modules/cars/useCases/listCategories"
 import { createCategoryController } from "../modules/cars/useCases/createCategory"
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-  return response.json({ all });
+  return listCategoriesController.handle(request, response);
 })
 
 export { categoriesRoutes } 
